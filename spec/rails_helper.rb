4d587f3751f4c2340f388b8bd1fe7dfc851e75c3
Paul Debevec <paulmdebevec@gmail.com>
2020-05-28 16:34:25 -0600

    Add Capybara, Simplecov and VCR to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,25 +7,29 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'pry'
 
-# Add additional requires below this line. Rails is not loaded until this point!
+Capybara.register_driver :selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Capybara.javascript_driver = :selenium_chrome_headless
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+Capybara.configure do |config|
+  config.default_max_wait_time = 5
+end
+
+SimpleCov.start 'rails'
+
+SimpleCov.start do
+  # add_filter 'app/channels'
+end
+
+Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -33,9 +37,12 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include FactoryBot::Syntax::Methods
 
   config.use_transactional_fixtures = true
 
@@ -44,7 +51,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
- 
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -74,4 +81,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<SPOTIFY-CLIENT-TOKEN-1>') { ENV['SPOTIFY_CLIENT_ID_1'] }
+  config.configure_rspec_metadata!
 end
