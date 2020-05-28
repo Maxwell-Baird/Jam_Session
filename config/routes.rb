056ca52902cb60a84a0e33aba6ae013ecc01c60c
Paul Debevec <paulmdebevec@gmail.com>
2020-05-28 16:35:33 -0600

    Add Spotify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/spotify', as: 'spotify_login'
+  get '/auth/:provider/callback', to: 'sessions#update'
 end
