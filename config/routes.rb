73216d584ef786b26c1700cf802aee7f360d5789
Colin Alexander <colintalex@gmail.com>
2020-05-28 18:57:13 -0600

    Creates route helpers for users and register path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/register', to: 'users#new'
-  post '/register', to: 'users#create'
+  post '/register', to: 'users#create', as: :users
 
+  resources :users, except: [:new, :create]
 end
