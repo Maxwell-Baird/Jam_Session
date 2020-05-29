e9b2cf2333cd9baf9fe097f4be47a6b029828f4b
Maxwell Baird <bairdm71@gmail.com>
2020-05-29 17:37:54 -0600

    added to the seed file to show full capablities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+StudySession.destroy_all
+User.destroy_all
+
+
+user = User.create(name: "bob", email: "faked", password: "password")
+user.studySessions.create(topic: "Ruby", duration: 5, paired: false)
+user.studySessions.create(topic: "Ruby", duration: 5, paired: true)
+user.studySessions.create(topic: "Cake", duration: 5, paired: true)
+user.studySessions.create(topic: "Cheese", duration: 5, paired: false)
+user.studySessions.create(topic: "Books", duration: 5, paired: false)
+user.studySessions.create(topic: "Books", duration: 5, paired: false)
