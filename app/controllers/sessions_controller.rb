ac55b1167780ec7b26501a012daed52c61d6fc70
Paul Debevec <paulmdebevec@gmail.com>
2020-05-29 12:39:42 -0600

    Add create, update, and private spotify_info methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,30 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to '/dashboard'
+    else
+      flash[:error] = 'Your email or password was incorrect'
+      render :new
+    end
+  end
+
+  def update
+    user = User.find_by(email: params[:session][:email])
+    expires = Time.now + 3600
+    user.update(spotify_token: spotify_info['credentials']['token'],
+                refresh_token: spotify_info['credentials']['refresh_token'],
+                token_expiration: expires )
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def spotify_info
+    request.env['omniauth.auth']
+  end
 end
