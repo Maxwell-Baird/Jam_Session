87c911994cd4756af7f0e45c14b8664f8dc770e5
Paul Debevec <paulmdebevec@gmail.com>
2020-05-29 17:07:05 -0600

    Add test to mock spotify login and to test the spotify_token is saved.

---
+++ spec/features/user/user_can_connect_with_spotify_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "As a logged in user" , type: :feature do
+  describe "when I click connect with spotify" do
+    before(:each) do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:spotify] = OmniAuth::AuthHash.new(
+        {"provider"=>"spotify",
+         "uid"=>"129075093",
+         "credentials"=>
+          {"token"=> ENV['SPOTIFY_TEMP_TOKEN'],
+           "refresh_token"=>ENV['SPOTIFY_TEMP_REFRESH'],
+           "expires_at"=>1590794669,
+           "expires"=>true}
+          })
+    end
+
+    it "My spotify token is saved to my account when I connect to spotify" do
+      VCR.use_cassette('spotify_authentication') do
+        user = User.create( name: 'Pablo Dee',
+                            email: 'test@example.com',
+                            password: 'password')
+
+
+        visit '/'
+        click_on 'Login'
+
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+
+        click_button 'Log In'
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit '/'
+        click_on 'Connect to Spotify'
+
+        expect(user.spotify_token).to eq(ENV['SPOTIFY_TEMP_TOKEN'])
+      end
+    end
+  end
+end
