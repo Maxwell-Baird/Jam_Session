0069596ed8d5a6b62d866c45be3c6eba4f7c7948
Paul Debevec <paulmdebevec@gmail.com>
2020-05-30 16:04:50 -0600

    Add test for user log out

---
+++ spec/features/user/logout_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'logging in' do
+  it 'can log in with valid credentials' do
+    user = User.create( name: 'Pablo Dee',
+                        email: 'test@example.com',
+                        password: 'password')
+
+    visit '/'
+
+    click_on 'Login'
+
+    expect(current_path).to eq('/login')
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log In'
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    expect(current_path).to eq('/dashboard')
+
+    click_link 'Logout'
+
+    expect(current_path).to eq('/')
+    expect(page).to have_content('Logout Successful')
+  end
+end
