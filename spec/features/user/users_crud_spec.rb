f84d9f092f671f14cf7a20e5f8c014f0bdc757b6
Colin Alexander <colintalex@gmail.com>
2020-05-31 02:53:28 -0600

    Unskips delete user test, creates edge test for user creation

--- spec/features/user/users_crud_spec.rb
+++ spec/features/user/users_crud_spec.rb
@@ -10,10 +10,35 @@ describe "As a guest user" do
     click_on 'Register'
 
     @user = User.last
-    expect(current_path).to eq(user_path(@user))
+    expect(current_path).to eq('/dashboard')
     expect(page).to have_content('Welcome New Name')
     expect(@user.name).to eq("New Name")
     expect(@user.email).to eq("user@example.com")
+  end
+
+  it "I see a flash message if I incorrectly fill out the form, and redirect to the register form" do
+    visit '/register'
+
+    fill_in 'Name', with: "New Name"
+    fill_in 'Email', with: "user@example.com"
+    click_on 'Register'
+
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Password can't be blank")
+
+    fill_in 'Name', with: "New Name"
+    fill_in 'Password', with: "password"
+    click_on 'Register'
+
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Email can't be blank")
+
+    fill_in 'Email', with: "user@example.com"
+    fill_in 'Password', with: "password"
+    click_on 'Register'
+
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Name can't be blank")
   end
 end
 
@@ -36,13 +61,15 @@ describe "As a registered user when I am logged in" do
 
   end
 
-  xit "I can delete my own user account, and redirect to the register page" do
+  it "I can delete my own user account, and redirect to the register page" do
     @user = User.create(name: "Bob", email: "bob@bob.com", password: "abcd")
+    @user2 = User.create(name: "Bobby", email: "bobby@bob.com", password: "abcd")
 
     visit user_path(@user)
 
     click_on 'Delete Profile'
 
     expect(current_path).to eql(register_path)
+    expect(User.last.id).to_not eql(@user.id)
   end
 end
