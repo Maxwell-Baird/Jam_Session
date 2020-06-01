bc8ddb76b4bee9899053de2c43f836bd767a21b7
Maxwell Baird <bairdm71@gmail.com>
2020-05-31 23:24:09 -0600

    fixed the view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User < ApplicationRecord
   has_secure_password
 
   def total_time
-    studySessions.sum(:duration)
+    time = studySessions.sum(:duration)
+    minutes_to_hours(time)
   end
 
   def paired_time
-    studySessions.where(paired: true).sum(:duration)
+    time = studySessions.where(paired: true).sum(:duration)
+    minutes_to_hours(time)
   end
 
   def topics
@@ -21,17 +23,28 @@ class User < ApplicationRecord
   end
 
   def topic_time(topic)
-    studySessions.where(topic: topic).sum(:duration)
+    time = studySessions.where(topic: topic).sum(:duration)
+    minutes_to_hours(time)
   end
 
   def paired_topic_time(topic)
-    studySessions.where(topic: topic, paired: true).sum(:duration)
+    time = studySessions.where(topic: topic, paired: true).sum(:duration)
+    minutes_to_hours(time)
   end
 
   def global_time
-    your_time = total_time * 100
+    your_time = studySessions.sum(:duration) * 100
     global = StudySession.sum(:duration)
-    your_time / global
+    number = your_time / global.to_f
+    number.round(2)
+  end
+
+  def minutes_to_hours(minutes_params)
+    hours = minutes_params / 60
+    minutes = minutes_params % 60
+    hours_string = hours.to_s.rjust(2, "0")
+    minutes_string = minutes.to_s.rjust(2, "0")
+    "#{hours_string} hours and #{minutes_string} minutes."
   end
 
 end
