8b05d4ebc1a4f035abd1000d8feaf856402d76ff
Paul Debevec <paulmdebevec@gmail.com>
2020-06-01 13:33:29 -0600

    Add functionality for visitors to login with spotify

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,11 +16,17 @@ class SessionsController < ApplicationController
 
   def update
     expires = Time.now + 3600
-    current_user.update(uid: spotify_info['uid'],
-                spotify_token: spotify_info['credentials']['token'],
-                refresh_token: spotify_info['credentials']['refresh_token'],
-                token_expiration: expires)
-
+    if current_user
+      current_user.update(uid: spotify_info['uid'],
+                  spotify_token: spotify_info['credentials']['token'],
+                  refresh_token: spotify_info['credentials']['refresh_token'],
+                  token_expiration: expires)
+    elsif user = User.find_by(uid: :spotify_info['uid'])
+      session[:user_id] = user.id
+      user.update(spotify_token: spotify_info['credentials']['token'],
+                  refresh_token: spotify_info['credentials']['refresh_token'],
+                  token_expiration: expires)
+    end
     redirect_to dashboard_path
   end
 
