94dbefa4adce88d0381e2d830a3328fb91684f15
DeMarcus Kirby <tkd024@gmail.com>
2020-06-01 13:55:45 -0600

    Add test for service

---
+++ spec/requests/search_result_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor to the About Page' do
+  it 'should show information' do
+    VCR.use_cassette('quote_cassette') do
+      result = SearchResults.new.get_quote
+      expect(result).to be_an_instance_of(Quote)
+    end
+  end
+end
