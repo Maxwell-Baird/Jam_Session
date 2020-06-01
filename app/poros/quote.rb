0d5d5d879c0820f3364179de68167f8ba8e213c8
DeMarcus Kirby <tkd024@gmail.com>
2020-06-01 15:54:18 -0600

    Update Quoto Poro

--- app/poros/quote.rb
+++ app/poros/quote.rb
@@ -2,6 +2,6 @@ class Quote
   attr_reader :text, :author
   def initialize(obj)
     @text = obj[:text]
-    @author = obj[:author] || "Author Unknown"
+    @author = obj[:author] || "Unknown Author"
   end
 end
