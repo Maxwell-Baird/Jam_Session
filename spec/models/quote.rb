df4b358258c5c34e1dfdd45f9b875f19bc4171ac
DeMarcus Kirby <tkd024@gmail.com>
2020-06-01 13:55:28 -0600

    Add test to quote poro

---
+++ spec/models/quote.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Quote, type: :model do
+  it 'should have attributes' do
+    quote = Quote.new({text: "This is a quote", author: "Tron"})
+    expect(quote.text).to eq("This is a quote")
+    expect(quote.author).to eq("Tron")
+  end
+
+  it 'should fill in attribute if no author' do
+    quote2 = Quote.new({text: "This is also a quote"})
+    expect(quote2.text).to eq("This is also a quote")
+    expect(quote2.author).to eq("Author Unknown")
+  end
+end
