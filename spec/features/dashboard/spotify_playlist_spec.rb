e00f304d1431eba732e1b6613895b31a4cf36665
Paul Debevec <paulmdebevec@gmail.com>
2020-06-03 14:41:27 -0600

    Add test for spotify playlist dropdown and widget

---
+++ spec/features/dashboard/spotify_playlist_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe 'As a logged in user who is connected with spotify' do
+  it 'I see a dropdown menu with my playlist names on my dashboard' do
+    VCR.use_cassette('spotify_cassette') do
+      user = User.create( name: 'Pablo Dee',
+                          email: 'test@example.com',
+                          password: 'password',
+                          spotify_token: ENV['SPOTIFY_TEMP_TOKEN'],
+                          refresh_token: ENV['SPOTIFY_TEMP_TOKEN'],
+                          token_expiration: Time.now + 3600)
+      visit '/'
+
+      click_on 'Login'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_on 'Log In'
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+      select 'World of Warcraft calm music', :from => 'playlist-select'
+      click_on "Play This!"
+
+      expect(current_path).to eq("/dashboard")
+      within '#spotify-embedded' do
+        expect(page).to have_content('World of Warcraft calm music')
+        expect(page).to have_content('Elwynn Forest')
+      end
+    end
+  end
+end
