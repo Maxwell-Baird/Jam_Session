2c2f26b3d78cefbdfff3b5123ad6b7145bcea4c1
Paul Debevec <paulmdebevec@gmail.com>
2020-06-03 11:38:52 -0600

    Add streaming scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_CLIENT_ID_1'], ENV['SPOTIFY_SECRET_1'],
-  scope: 'streaming playlist-read-private user-read-private user-read-email user-read-currently-playing'
+  scope: 'streaming playlist-read-collaborative playlist-read-private user-read-private user-read-email user-read-currently-playing'
 end
