926118377b59c7727d1c2b2e16d9419409e8fbfb
Paul Debevec <paulmdebevec@gmail.com>
2020-06-03 18:24:33 -0600

    Add Playlist poro and selected_playlist method to return the playlist uri

---
+++ app/poros/playlist.rb
@@ -0,0 +1,16 @@
+class Playlist
+  attr_reader :url, :name, :track_count
+
+  def initialize(playlist_data)
+    @url = playlist_data[:url].insert(25, 'embed/')
+    @name = playlist_data[:name]
+    @track_count = playlist_data[:track_count]
+  end
+
+  def self.selected_playlist(playlists, selection)
+    playlist = playlists.find do |playlist|
+      selection == playlist.name
+    end
+    playlist.url
+  end
+end
